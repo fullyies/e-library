1c72c9f92847e69a5aa809fbf362da7708d04fe9
Fullyies <randigans2@gmail.com>
2026-07-02 10:46:59 +0700

    edit web.php and add view resources

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\KategoriController;
+use App\Http\Controllers\BukuController;
+use App\Http\Controllers\PeminjamanController;
+
+Route::get('/', [DashboardController::class,'index']);
+
+Route::resource('kategori',KategoriController::class);
+
+Route::resource('buku',BukuController::class);
+
+Route::resource('peminjaman',PeminjamanController::class);
